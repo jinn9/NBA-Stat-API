0285ffb05b9303672f719a27983d72dee62a55ce
Jin Jung <Jin@Jins-MacBook-Pro.local>
2025-02-18 21:17:36 -0500

    Commit the project

---
+++ src/main/java/com/github/jinn9/nba_api/NbaApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.jinn9.nba_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jinn9/nba_api/NbaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jinn9.nba_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
